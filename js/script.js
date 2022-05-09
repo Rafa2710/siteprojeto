76e18b649be16f281428b45d9745037128956fdc
rafael <rafa548017@gmail.com>
2022-05-09 11:40:45 -0300

    site projeto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function laranja(){
+    document.getElementById("estadio").src="img/setor_laranja.png";
+}
+function azul(){
+    document.getElementById("estadio").src="img/setor_azul.png";
+}
+
+function vermelha(){
+    document.getElementById("estadio").src="img/setor_vermelho.png";
+}
+
+function amarelo(){
+    document.getElementById("estadio").src="img/setor_amarelo.png";
+}
+
+
+
+
 
